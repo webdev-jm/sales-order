ae23a5a6d2e5a962f5abeac2bed3c5f27c6a36d3
John Michael Reñon <michael.renon@Makati.local>
2022-09-05 09:02:52 +0800

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('adminlte::page')
+
+@section('title')
+    Home
+@endsection
+
+@section('css')
+@endsection
+
+@section('content_header')
+    <h1>Home</h1>
+@endsection
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Home</h3>
+    </div>
+    <div class="card-body">
+
+    </div>
+</div>
+@endsection
+
+@section('js')
+@endsection
+
+@section('footer')
+@endsection
+
+@section('right-sidebar')
+sidebar
+@endsection
