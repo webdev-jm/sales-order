0261c95db13e0ef7104d939a4e689aec785acc13
John Michael Reñon <michael.renon@Makati.local>
2023-02-02 15:01:10 +0800

    weekly activity plan updates

--- app/Http/Requests/StoreWeeklyActivityReportRequest.php
+++ app/Http/Requests/StoreWeeklyActivityReportRequest.php
@@ -62,60 +62,6 @@ class StoreWeeklyActivityReportRequest extends FormRequest
             'highlights' => [
                 'required', 'max:1000'
             ],
-            // collections
-            'beginning_ar' => [
-                'required'
-            ],
-            'due_for_collection' => [
-                'required'
-            ],
-            'beginning_hanging_balance' => [
-                'required'
-            ],
-            'target_reconciliations' => [
-                'required'
-            ],
-            'week_to_date' => [
-                'required'
-            ],
-            'month_to_date' => [
-                'required'
-            ],
-            'month_target' => [
-                'required'
-            ],
-            'balance_to_sell' => [
-                'required'
-            ],
-            // action plans
-            'action_plan.*' => [
-                'max:255'
-            ],
-            'time_table.*' => [
-                'max:255'
-            ],
-            'person_responsible.*' => [
-                'max:255'
-            ],
-            // activities
-            'activity.*' => [
-                'required'
-            ],
-            'no_of_days_weekly.*' => [
-                'max:11'
-            ],
-            'no_of_days_mtd.*' => [
-                'max:11'
-            ],
-            'activity_remarks.*' => [
-                'max:255'
-            ],
-            'no_of_days_ytd.*' => [
-                'max:11'
-            ],
-            'total_working_days' => [
-                'max:255'
-            ]
         ];
     }
 }
--- resources/views/war/pdf.blade.php
+++ resources/views/war/pdf.blade.php
@@ -139,11 +139,6 @@
                 <th class="bg-gray">WEEK</th>
                 <td>Week {{$weekly_activity_report->week_number}}</td>
             </tr>
-            <tr>
-                <th class="bg-gray">AREA Visited</th>
-                <td>[{{$weekly_activity_report->area->area_code}}] {{$weekly_activity_report->area->area_name}}</td>
-                <td class="border-0" colspan="3"></td>
-            </tr>
         </thead>
     </table>
 
@@ -205,116 +200,6 @@
                 </td>
             </tr>
         </thead>
-    </table>
-
-    <table class="table table-sm">
-        <thead>
-            <tr class="bg-dark">
-                <th class="text-center">BEGINNING AR</th>
-                <th class="text-center">DUE FOR COLLECTION</th>
-                <th class="text-center">BEGINNING HANGING BALANCE</th>
-                <th class="text-center">TARGET RECONCILIATIONS</th>
-            </tr>
-            <tr>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->beginning_ar)}}</td>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->due_for_collection)}}</td>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->beginning_hanging_balance)}}</td>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->target_reconciliations)}}</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-dark">
-                <th class="text-center">WEEK TO DATE</th>
-                <th class="text-center">MONTH TO DATE</th>
-                <th class="text-center">MONTH TARGET</th>
-                <th class="text-center">BALANCE TO SELL</th>
-            </tr>
-            <tr>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->week_to_date)}}</td>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->month_to_date)}}</td>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->month_target)}}</td>
-                <td class="text-center">{{number_format($weekly_activity_report->collection->balance_to_sell)}}</td>
-            </tr>
-        </tbody>
-    </table>
-
-    <table class="table table-sm">
-        <thead>
-            <tr>
-                <th class="bg-gray" colspan="3">IV. SALES Action Plans (to achieve sales/collection targets/to accomplish a project):</th>
-            </tr>
-            <tr class="bg-dark">
-                <th class="text-center">ACTION PLAN/S</th>
-                <th class="text-center">TIMETABLE</th>
-                <th class="text-center">PERSON/S RESPONSIBLE</th>
-            </tr>
-        </thead>
-        <tbody>
-            @if(!empty($weekly_activity_report->action_plans))
-                @foreach($weekly_activity_report->action_plans as $action_plan)
-                <tr>
-                    <td>{{$action_plan->action_plan}}</td>
-                    <td class="text-center">{{$action_plan->time_table}}</td>
-                    <td class="text-center">{{$action_plan->person_responsible}}</td>
-                </tr>
-                @endforeach
-            @else
-                <tr>
-                    <td class="text-center" colspan="3">NO DATA</td>
-                </tr>
-            @endif
-        </tbody>
-    </table>
-
-    <table class="table table-sm">
-        <thead>
-            <tr class="bg-dark">
-                <th class="text-center">ACTIVITY</th>
-                <th class="text-center">NO OF DAYS (WEEKLY)</th>
-                <th class="text-center">NO OF DAYS (MTD)</th>
-                <th class="text-center">AREA/REMARKS</th>
-                <th class="text-center">NO OF DAYS (YTD)</th>
-                <th class="text-center">% TO TOTAL WORKING DAYS</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $total_weekly = 0;
-                $total_mtd = 0;
-                $total_ytd = 0;
-            @endphp
-            @if(!empty($weekly_activity_report->activities))
-                @foreach($weekly_activity_report->activities as $activity)
-                    @php
-                        $total_weekly += $activity->no_of_days_weekly;
-                        $total_mtd += $activity->no_of_days_mtd;
-                        $total_ytd += $activity->no_of_days_ytd;
-                    @endphp
-                    <tr>
-                        <td>{{$activity->activity}}</td>
-                        <td class="text-center">{{$activity->no_of_days_weekly}}</td>
-                        <td class="text-center">{{$activity->no_of_days_mtd}}</td>
-                        <td class="text-center">{{$activity->remarks}}</td>
-                        <td class="text-center">{{$activity->no_of_days_ytd}}</td>
-                        <td class="text-center">{{$activity->percent_to_total_working_days}}</td>
-                    </tr>
-                @endforeach
-            @else
-                <tr>
-                    <td colspan="6" class="text-center">NO DATA</td>
-                </tr>
-            @endif
-        </tbody>
-        <tfoot>
-            <tr>
-                <th class="text-center border-0">TOTAL</th>
-                <th class="text-center bl-0 br-0">{{$total_weekly}}</th>
-                <th class="text-center bl-0 br-0">{{$total_mtd}}</th>
-                <td class="border-0"></td>
-                <th class="text-center bl-0 br-0">{{$total_ytd}}</th>
-                <td class="border-0"></td>
-            </tr>
-        </tfoot>
     </table>
 
     @php
