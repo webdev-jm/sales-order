bbe023f5d3587c68fe44841a932e5a3fcd6da02a
John Michael Reñon <michael.renon@Makati.local>
2023-05-19 08:56:31 +0800

    user view page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('group_code')->nullable();
+            $table->string('status')->default('active')->nullable();
             $table->rememberToken();
             $table->timestamp('last_activity')->nullable();
             $table->timestamps();
